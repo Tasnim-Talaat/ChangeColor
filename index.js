c1fe511a4363329cef69d4b099eeea0c7f86b7c9
Tasnim <tasnim.talaat23@gmail.com>
2023-06-13 23:29:36 +0300

    change

---
+++ index.js
@@ -0,0 +1,7 @@
+input=document.getElementById('input')
+function changeColor(){
+        change=input.value
+        // console.log(change)
+        para=document.getElementById('para').style.color=change
+}
+
